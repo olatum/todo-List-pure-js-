46be28754a2ce685faf529bcda2bb566ef6c5e0c
Aleksandra Tumilowicz <olatum@wp.pl>
2018-06-07 23:15:58 +0200

    todo List

---
+++ app.js
@@ -0,0 +1,28 @@
+const list = document.getElementById('todoList');
+const addBtn = document.querySelector('#add-btn');
+const rmBtn = document.querySelector('#rm-btn');
+const clearBtn = document.querySelector('#clear-btn');
+
+addBtn.addEventListener('click', todoList);
+rmBtn.addEventListener('click', removeFromList);
+clearBtn.addEventListener('click', clearAllList);
+
+let counter = 1
+
+function todoList() {
+    const newItem = document.createElement('li')
+    let textnode = document.createTextNode("zadanie " + counter++)
+    newItem.appendChild(textnode)
+    document.getElementById('todoList').appendChild(newItem)
+}
+
+function removeFromList() {
+    list.removeChild(list.lastChild);
+}
+
+function clearAllList() {
+    const myNode = document.getElementById("todoList");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+}
